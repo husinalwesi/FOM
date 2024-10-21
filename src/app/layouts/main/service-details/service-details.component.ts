8c5b96683a517a41304b4bbc0bb1c881acded9da
Hussein Al-wesi <alwesihusin@gmail.com>
2024-10-21 16:37:48 +0300

    fixs

--- src/app/layouts/main/service-details/service-details.component.ts
+++ src/app/layouts/main/service-details/service-details.component.ts
@@ -5,12 +5,28 @@ import { MetaTagsService } from 'src/app/services/meta-tags.service';
 import { PageTransitionsService } from 'src/app/services/page-transitions.service';
 import { SharedService } from 'src/app/services/shared.service';
 
+
+
+
+import { ChangeDetectorRef, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { ResizeService } from 'src/app/services/resize.service';
+import { RouteLocalizationPipe } from "src/app/pipes/route-localization.pipe";
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/modules';
+import { ViewEncapsulation, HostListener, Output, EventEmitter } from '@angular/core';
+import { LoadAssetsService } from 'src/app/load-assets.service';
+import { TranslationService } from 'src/app/i18n/translation.service';
+
+
 @Component({
   selector: 'app-service-details',
   templateUrl: './service-details.component.html',
   styleUrls: ['./service-details.component.scss']
 })
 export class ServiceDetailsComponent {
+  swiper: any = null;
   @ViewChild('carouselServiceDetailsComponent') carouselServiceDetailsComponent!: CarouselServiceDetailsComponent;
   services: any = [1, 2, 3, 4, 5, 6, 7, 8];
   activeService: number = 0;
@@ -19,8 +35,10 @@ export class ServiceDetailsComponent {
     private pageTransitionsService: PageTransitionsService,
     private headerService: HeaderService,
     private metaTagsService: MetaTagsService,
-    private SharedService: SharedService
+    private SharedService: SharedService,
+    private loadAssetsService: LoadAssetsService,
   ) {
+    this.loadAssetsService.loadCss('assets/css/swiper-bundle.min.css', 'carousel-default');
     this.pageTransitionsService.HideLoad();
     this.headerService.useStickyHeader(false);
 
@@ -35,9 +53,57 @@ export class ServiceDetailsComponent {
     this.SharedService.navigateTo(route);
   }
 
+  ngOnInit(): void {
+    this.initializeCarousel();
+  }
+
   setSlide(slideNo: number) {
     this.activeService = slideNo;
     this.carouselServiceDetailsComponent.setSlide(slideNo);
+  }
+
+
+  initializeCarousel() {
+    if (typeof window !== 'undefined') {
+      // this.calculateRatio();
+      // https://swiperjs.com/swiper-api
+      this.swiper = new Swiper('.service-details-items-swiper-container', {
+
+        // effect: 'fade',
+        // fadeEffect: {
+        //   crossFade: true
+        // },
+        //   slideShadows: false,
+        // },
+
+
+        // modules: [Navigation, Pagination, Scrollbar],
+        // modules: [Navigation, Pagination],
+        updateOnWindowResize: true,
+        modules: [Navigation, Autoplay, Pagination],
+        slidesPerView: 4,
+        // width: 204,
+        spaceBetween: 16,
+        navigation: {
+          nextEl: '.swiper-service-details-items-button-next',
+          prevEl: '.swiper-service-details-items-button-prev',
+        },
+        // autoplay: {
+        //   delay: 3000
+        // },
+        pagination: {
+          el: '.swiper-service-details-items-pagination',
+          clickable: true,
+        },
+        // loop: true,
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+        // resize: () => {
+
+        // }
+      });
+    }
   }
 
 }
--- src/app/modules/home-blogs/home-blogs.component.ts
+++ src/app/modules/home-blogs/home-blogs.component.ts
@@ -3,6 +3,12 @@ import { Router } from '@angular/router';
 import { ResizeService } from 'src/app/services/resize.service';
 import { RouteLocalizationPipe } from "src/app/pipes/route-localization.pipe";
 import { PageTransitionsService } from 'src/app/services/page-transitions.service';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/modules';
+import { SharedService } from 'src/app/services/shared.service';
+import { ViewChild, ViewEncapsulation, HostListener, Output, EventEmitter } from '@angular/core';
+import { LoadAssetsService } from 'src/app/load-assets.service';
+import { TranslationService } from 'src/app/i18n/translation.service';
 
 @Component({
   selector: 'app-home-blogs',
@@ -11,6 +17,7 @@ import { PageTransitionsService } from 'src/app/services/page-transitions.servic
   providers: [RouteLocalizationPipe]
 })
 export class HomeBlogsComponent {
+  swiper: any = null;
   news: any = [1, 2, 3];
   heightNews: number = 0;
   isMobile: boolean = false;
@@ -21,8 +28,13 @@ export class HomeBlogsComponent {
     private router: Router,
     private RouteLocalizationPipe: RouteLocalizationPipe,
     private pageTransitionsService: PageTransitionsService,
-    private ngZone: NgZone
-  ) { }
+    private ngZone: NgZone,
+    private loadAssetsService: LoadAssetsService,
+    private TranslationService: TranslationService,
+    private SharedService: SharedService
+  ) {
+    this.loadAssetsService.loadCss('assets/css/swiper-bundle.min.css', 'carousel-default');
+  }
 
   ngAfterViewInit(): void {
     this.resizeService.screenWidthChange$.subscribe(data => {
@@ -31,12 +43,26 @@ export class HomeBlogsComponent {
     });
   }
 
+  ngOnInit(): void {
+    this.initializeCarousel();
+  }
+
   newsRatio() {
     const desktopRatio: number = 242 / 286;
     const mobileRatio: number = 242 / 286;
     this.isMobile = this.resizeService.isMobile();
     const detectRatio = this.isMobile ? mobileRatio : desktopRatio;
-    const mobileSectionEle: any = typeof window !== 'undefined' ? document.querySelector(".news-item") : null;
+
+    const mobileSectionEleAll: any = typeof window !== 'undefined' ? document.querySelectorAll(".news-item") : null;
+    let mobileSectionEle = null;
+
+    for (let index = 0; index < mobileSectionEleAll.length; index++) {
+      if (mobileSectionEleAll[index].offsetWidth !== 0) {
+        mobileSectionEle = mobileSectionEleAll[index];
+        break;
+      }
+    }
+
     if (!mobileSectionEle) return;
     const screenWidth = mobileSectionEle.offsetWidth;
     this.heightNews = screenWidth / detectRatio;
@@ -48,6 +74,49 @@ export class HomeBlogsComponent {
         this.router.navigateByUrl(this.RouteLocalizationPipe.transform(`/blogs`));
       });
     });
+  }
+
+  initializeCarousel() {
+    if (typeof window !== 'undefined') {
+      // this.calculateRatio();
+      // https://swiperjs.com/swiper-api
+      this.swiper = new Swiper('.blogs-swiper-container', {
+
+        // effect: 'fade',
+        // fadeEffect: {
+        //   crossFade: true
+        // },
+        //   slideShadows: false,
+        // },
+
+
+        // modules: [Navigation, Pagination, Scrollbar],
+        // modules: [Navigation, Pagination],
+        updateOnWindowResize: true,
+        modules: [Navigation, Autoplay, Pagination],
+        slidesPerView: 1.2,
+        // width: 204,
+        spaceBetween: 16,
+        navigation: {
+          nextEl: '.swiper-blogs-button-next',
+          prevEl: '.swiper-blogs-button-prev',
+        },
+        // autoplay: {
+        //   delay: 3000
+        // },
+        pagination: {
+          el: '.swiper-blogs-pagination',
+          clickable: true,
+        },
+        // loop: true,
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+        // resize: () => {
+
+        // }
+      });
+    }
   }
 
 }
